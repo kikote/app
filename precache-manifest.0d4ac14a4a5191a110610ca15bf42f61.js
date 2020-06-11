c57e8c41b436cd034c9af43dd405a311f7579d80
pvernier <pvernier@users.noreply.github.com>
2020-06-11 22:11:58 +0000

    deploy: 66cee9d69e4d92a5a84d40d80f05119d1ca5e0a6

---
+++ precache-manifest.0d4ac14a4a5191a110610ca15bf42f61.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0da79c0e1007e5a00b6c4940ec7b69f",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "478fece0066a1f536529",
+    "url": "/app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "56c795c88cf82de4d912",
+    "url": "/app/static/js/2.78226258.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/app/static/js/2.78226258.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "478fece0066a1f536529",
+    "url": "/app/static/js/main.149ec55a.chunk.js"
+  },
+  {
+    "revision": "9cd21ffa4e09cf74ff81",
+    "url": "/app/static/js/runtime-main.e2a5f235.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/app/static/media/logo.5d5d9eef.svg"
+  }
+]);
