e90313ccdd298aa0544fed40d4ef3ec848e5f2b5
pvernier <pvernier82@gmail.com>
2020-06-18 20:55:49 +0100

    Change layout from flexbox to grid

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -3,7 +3,15 @@ import React from "react";
 import "./Toolbar.scss";
 
 const Toolbar = (props) => {
-  return <div className="toolbar"></div>;
+  return (
+    <div className="toolbar">
+      <div className="toolbar-header"></div>
+      <div className="toolbar-body"></div>
+      <div className="toolbar-footer">
+        Version: {process.env.REACT_APP_VERSION}
+      </div>
+    </div>
+  );
 };
 
 export default Toolbar;
