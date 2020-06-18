f42166f6be0bd8b73ff532e08869b8534e2fe135
pvernier <pvernier82@gmail.com>
2020-06-19 00:29:46 +0100

    Make layer elements draggable - WIP

--- src/components/SideBar/LayersList/LayersList.js
+++ src/components/SideBar/LayersList/LayersList.js
@@ -1,15 +1,16 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { connect } from "react-redux";
 
 import "./LayersList.scss";
 
 const LayersList = (props) => {
   console.log("RENDERING LayersList");
-  //   useEffect(() => {
-  //     console.log("New layer!");
-  //   }, [props.layers]);
-  console.log(props.layers);
-  const layersElement = props.layers.map((l, i) => <li key={i}>{l.name}</li>);
+
+  const layersElement = props.layers.map((l, i) => (
+    <li key={i} draggable="true">
+      {l.name}
+    </li>
+  ));
   return <ul>{layersElement}</ul>;
 };
 
